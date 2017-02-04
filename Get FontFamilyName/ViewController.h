77f3aa6ca37f22e9c750c6d900065d2a82f228a9
ferrakkem <ferrakkem@gmail.com>
2017-02-04 20:08:27 +0600

    Initial Commit

---
+++ Get FontFamilyName/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Get FontFamilyName
+//
+//  Created by Ferrakkem Bhuiyan on 4/2/17.
+//  Copyright © 2017 Ferrakkem Bhuiyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Get FontFamilyName/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Get FontFamilyName
+//
+//  Created by Ferrakkem Bhuiyan on 4/2/17.
+//  Copyright © 2017 Ferrakkem Bhuiyan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
